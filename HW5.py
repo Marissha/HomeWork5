0a0c20e30e5ff919497779feb99ca69e7f463972
Marissha <balabobalaba71@gmail.com>
2024-07-24 11:12:23 +0300

    Add files via upload

---
+++ HW5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 2, 0, True, 'date'
+print('Immutable tuple: ', immutable_var)
+#immutable_var[0] = 5
+#print(immutable_var) # невозможно изменять значение элементов кортежа
+muttable_var = [2, 1, 0, False, 'file']
+print('Mutable list: ', muttable_var)
